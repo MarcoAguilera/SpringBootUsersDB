1dedd1d18c8c996e1b087d7b0592f26160e225e7
Marco Aguilera <maaguilera@csumb.edu>
2018-11-27 22:03:59 -0800

    Added some default

--- src/main/java/com/example/springbootmongodbexample/config/MongoDbConfig.java
+++ src/main/java/com/example/springbootmongodbexample/config/MongoDbConfig.java
@@ -1,18 +1,23 @@
-// package com.example.springbootmongodbexample.config;
-// import com.example.springbootmongodbexample.document.Users;
-// import com.example.springbootmongodbexample.repo.UsersRepository;
+package com.example.springbootmongodbexample.config;
+import com.example.springbootmongodbexample.document.Users;
+import com.example.springbootmongodbexample.repo.UsersRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 
 
-// @EnableMongoRepositories(basePackageClasses = UsersRepository.class)
-// @Configuration
-// public class MongoDbConfig {
-//     @Bean
-//     CommandLineRunner commandLineRunner(UsersRepository usersRepository ) {
-//         return args -> {
-//             usersRepository.save(new Users("Peter123", "Development"));
-//             usersRepository.save(new Users("admin", "password"));
-//         };
-//     }
-// }
+@EnableMongoRepositories(basePackageClasses = UsersRepository.class)
+@Configuration
+public class MongoDbConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(UsersRepository usersRepository ) {
+        return args -> {
+            usersRepository.save(new Users("Peter123", "Development"));
+            usersRepository.save(new Users("admin", "password"));
+        };
+    }
+}
