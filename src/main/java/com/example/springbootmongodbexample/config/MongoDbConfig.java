e19dc8e16f492d4cd996e520f46432190e936eed
Marco Aguilera <maaguilera@csumb.edu>
2018-12-02 10:29:17 -0800

    Bean Activated

--- src/main/java/com/example/springbootmongodbexample/config/MongoDbConfig.java
+++ src/main/java/com/example/springbootmongodbexample/config/MongoDbConfig.java
@@ -1,30 +1,30 @@
-// package com.example.springbootmongodbexample.config;
+package com.example.springbootmongodbexample.config;
 
-// import com.example.springbootmongodbexample.cart.CartItem;
-// import com.example.springbootmongodbexample.document.Users;
-// import com.example.springbootmongodbexample.repo.UsersRepository;
+import com.example.springbootmongodbexample.cart.CartItem;
+import com.example.springbootmongodbexample.document.Users;
+import com.example.springbootmongodbexample.repo.UsersRepository;
 
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
-// @EnableMongoRepositories(basePackageClasses = UsersRepository.class)
-// @Configuration
-// public class MongoDbConfig {
-//     @Bean
-//     CommandLineRunner commandLineRunner(UsersRepository usersRepository ) {
-//         return args -> {
+
+
+@EnableMongoRepositories(basePackageClasses = UsersRepository.class)
+@Configuration
+public class MongoDbConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(UsersRepository usersRepository ) {
+        return args -> {
             
-//                 usersRepository.deleteAll();
-//                 Users t1 = new Users("Peter123", "Development");
-//                 Users t2 = new Users("admin", "password");
-//                 usersRepository.save(t1);
-//                 usersRepository.save(t2);
+                usersRepository.deleteAll();
+                Users t1 = new Users("Peter123", "Development");
+                Users t2 = new Users("admin", "password");
+                usersRepository.save(t1);
+                usersRepository.save(t2);
 
-//         };
-//     }
-// }
+        };
+    }
+}
