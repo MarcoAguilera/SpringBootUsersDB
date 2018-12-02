ac50cb453841b4f00a726dcebcdec4f3d0448985
Marco Aguilera <maaguilera@csumb.edu>
2018-12-02 10:23:39 -0800

    Fixed Cart Item Class

--- src/main/java/com/example/springbootmongodbexample/cart/CartItem.java
+++ src/main/java/com/example/springbootmongodbexample/cart/CartItem.java
@@ -1,30 +1,68 @@
 package com.example.springbootmongodbexample.cart;
 
 public class CartItem {
-    private String Id;
-    private int quantity;
+    private String id;
+    private String name;
+    private double price;
+    private String image;
+    private String description;
+    private int stock;
 
 
-    public CartItem(String Id, int quantity) {
-        this.Id = Id;
-        this.quantity = quantity;
+    public CartItem(String id, String name, double price, String image, String description, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.image = image;
+        this.description = description;
+        this.stock = stock;
     }
 
     public String getId() {
-        return Id;
+        return this.id;
     }
 
-    public void setId(String Id) {
-        this.Id = Id;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    
-    public int getQuantity() {
-        return quantity;
+    public String getName() {
+        return this.name;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public void setName(String name) {
+        this.name = name;
     }
 
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
